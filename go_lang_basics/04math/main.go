c31bd1e91b43c3aea4d2c35229d3c969b4053889
bhavi dhingra1 <bhavi.dhingra1@SW-LP03704.local>
2020-07-22 19:55:15 +0530

    Adding Day 3 changes

--- go_lang_basics/04math/main.go
+++ go_lang_basics/04math/main.go
@@ -10,7 +10,12 @@ func main()  {
 	i1, i2, i3 := 10, 20, 30
 	intSum := i1 + i2 + i3
 
-	fmt.Println("", intSum)
+	fmt.Println("Sum of Number " , intSum)
+
+	f1, f2, f3 := 23.4, 43.5, 677.6
+	floatSum := f1 + f2 + f3
+
+	fmt.Println("Sum of floats ", floatSum)
 
 	var b1, b2, b3, bigSum big.Float
 	b1.SetFloat64(1.1)
@@ -18,9 +23,12 @@ func main()  {
 	b3.SetFloat64(3.6)
 
 	bigSum.Add(&b1, &b2).Add(&bigSum, &b3)
-	fmt.Printf("BigSum value: %.2f\n", &bigSum)
+	fmt.Printf("BigSum value : %.2f\n", &bigSum)
 
-	cirleRadius := 14.5
-	circumference := cirleRadius * math.Pi
-	fmt.Printf("%f", circumference)
+	circleRadius:=14.5
+	circumference := circleRadius * math.Pi
+	fmt.Printf("Circumference: %.2f", circumference)
+
+
+
 }
