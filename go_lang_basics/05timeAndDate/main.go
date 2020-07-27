19aa4d222afc72bee45e102041285d52aebefd4a
bhavi dhingra1 <bhavi.dhingra1@SW-LP03704.local>
2020-07-27 14:26:54 +0530

    Add Week 1 files

--- go_lang_basics/05timeAndDate/main.go
+++ go_lang_basics/05timeAndDate/main.go
@@ -5,26 +5,33 @@ import (
 	"time"
 )
 
-func main()  {
-	t := time.Date(2020, time.July, 21, 10, 10, 10, 10, time.UTC)
-	fmt.Println("Time is %s \n", t)
+func main() {
+	t:= time.Date(2020,time.July, 20,10,10,10,10,time.UTC)
+	fmt.Printf("Time is %s \n", t)
 
-	now := time.Now()
+	now:= time.Now();
 	fmt.Printf("Time Now is : %s\n", now)
 
-	// Month
-	fmt.Println("Month: ", t.Month())
+	// month
+	fmt.Println("Month : ", t.Month())
 
 	// Day
-	fmt.Println("Dat: ", t.Day())
+	fmt.Println("Day: " , t.Day())
 
 	// Week Day
-	fmt.Println("Week Day:", t.Weekday())
+	fmt.Println("Week Day :", t.Weekday())
 
-	tomorrow := t.AddDate(0, 0, 1)
+	tomorrow:=t.AddDate(0,0,1)
 	fmt.Printf("Tomorrow: %s\n", tomorrow)
 
-	fmt.Printf("Tomorrow Date: %v, Month: %v\n", tomorrow.Day(), tomorrow.Month())
+	fmt.Printf("Tomorrow Date: %v, Month %v\n", tomorrow.Day(), tomorrow.Month())
 
-	fmt.Printf("Tomorrow Date: %s", tomorrow.Format("01/02/2006"))
+	fmt.Println("RFC850 : ", tomorrow.Format(time.RFC850))
+	fmt.Println("RFC1123 : ",tomorrow.Format(time.RFC1123))
+
+	// 7/22/2020
+	fmt.Println(tomorrow.Format("Jan 2, 2006 at 3:04pm (MST)"))
+
+	fmt.Println(tomorrow.Format("Monday, Jan"))
+
 }
