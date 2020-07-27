dd3441d7ae0d5a5c195ed6f4b2697ab31a0b67a9
bhavi dhingra1 <bhavi.dhingra1@SW-LP03704.local>
2020-07-27 14:31:33 +0530

    Revert "Adding Day 3 changes"
    This reverts commit c31bd1e91b43c3aea4d2c35229d3c969b4053889.

--- go_lang_basics/consoleinput/main.go
+++ go_lang_basics/consoleinput/main.go
@@ -9,39 +9,32 @@ import (
 )
 
 func main()  {
-
-	//var s string
+	// var s string
 	// if you have a long string like (hi how are you) then only hi is considered
-	//fmt.Scanln(&s)
-	//fmt.Println(s)
+	// fmt.Scanln(&s)
+	// fmt.Println(s)
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter Your Name")
+	fmt.Println("Enter You Name")
 	name, _ := reader.ReadString('\n')
-	fmt.Println("Your Name is " , name)
+
+	fmt.Println("Your Name is", name)
 
 	// accept salary -> float
-	fmt.Println("Enter Your Salary")
-	strSalary, _ := reader.ReadString('\n')
-	salary, _  := strconv.ParseFloat(strings.TrimSpace(strSalary), 64);
-	fmt.Println("Your Salary is " , salary)
-
 	// accept number of months since joined -> int
-	fmt.Println("Enter Number of Months")
-	strMonths, _ := reader.ReadString('\n')
-	months, _  := strconv.ParseInt(strings.TrimSpace(strMonths), 10, 64)
-	fmt.Println("Number Of Months You Worked: " , months)
-
-
-
-
-
-
-
-
-
 	// calculate salary * noOfMonths
 
+	var salary float64
+	var numMonths int64
 
+	fmt.Println("Enter Your Salary")
+	sSalary, _ := reader.ReadString('\n')
+	salary, _ = strconv.ParseFloat(strings.TrimSpace(sSalary), 64)
+	fmt.Println("You Salary:", salary)
+
+	fmt.Println("Enter Num Months")
+	sNumMonths, _ := reader.ReadString('\n')
+	numMonths, _ = strconv.ParseInt(strings.TrimSpace(sNumMonths), 10, 32)
+	fmt.Println("Num of months:", numMonths)
 
 }
